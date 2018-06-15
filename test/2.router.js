ebf30b5ab2e1995faebce358e7fe25d1de07e133
[xjylyh] <18001300327@163.com>
2018-06-15 18:30:52 +0800

    first commit

---
+++ test/2.router.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+app.get('/',function(req,res,next){
+    console.log(1);
+    next();
+},function(req,res,next){
+    console.log(11);
+    next();
+}).get('/',function(req,res,next){
+    console.log(2);
+    next();
+}).get('/',function(req,res,next){
+    console.log(3);
+    res.end('ok');
+}).get('/',function(err,req,res,next){
+    res.end('catch: '+err);
+});
+app.listen(3000,function(){
+    console.log('server started on port 3000');
+});
